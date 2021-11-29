62401b08f5bd66537a6edd5f28aabd3bedef6b35
김우재 <kwj1270@naver.com>
2021-11-29 16:34:12 +0900

    step1: 예제 프로젝트 만들기

---
+++ src/main/kotlin/hello/advanced/SpringCoreAdvancedApplication.kt
@@ -0,0 +1,11 @@
+package hello.advanced
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCoreAdvancedApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCoreAdvancedApplication>(*args)
+}
